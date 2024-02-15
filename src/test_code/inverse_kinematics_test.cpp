06466dd845468afd6de382076826605e2023788b
jingerchong <jingersiachong@gmail.com>
2024-02-15 11:30:04 -0500

    added commented out code to make ik error 0 always

--- src/test_code/inverse_kinematics_test.cpp
+++ src/test_code/inverse_kinematics_test.cpp
@@ -7,10 +7,12 @@
 
 JointSpace state;
 JointSpace test_state;
+JointSpace alt_test_state;
+TaskSpace point;
 
 // Encoder velocity readers
 EncoderVelocity encoder1(ENCODER1_A_PIN, ENCODER1_B_PIN, CPR_60_RPM);
-EncoderVelocity encoder2(ENCODER2_A_PIN, ENCODER2_B_PIN, CPR_60_RPM);
+EncoderVelocity encoder2(ENCODER2_B_PIN, ENCODER2_A_PIN, CPR_60_RPM);
 
 void setup() {
     Serial.begin();
@@ -20,10 +22,20 @@ void setup() {
 void printExpectedJoint(){
     state.theta1 = encoder1.getPosition() + THETA1_OFFSET;
     state.theta2 = -encoder2.getPosition();
-    test_state = inverseKinematics(forwardKinematics(state));
+    point = forwardKinematics(state);
+    test_state = inverseKinematics(point);
     Serial.printf("theta1_error: %.2f, theta2_error: %.2f\n", 
                   abs(state.theta1 - test_state.theta1),
                   abs(state.theta2 - test_state.theta2));
+    
+    // alt_test_state.theta1 = 2*atan2(point.y, point.x) - test_state.theta1;
+    // alt_test_state.theta2 = -test_state.theta2; 
+
+    // Serial.printf("theta1_error: %.2f, theta2_error: %.2f\n", 
+    //               min(abs(state.theta1 - test_state.theta1),
+    //               abs(state.theta1 - alt_test_state.theta1)),
+    //               min(abs(state.theta2 - test_state.theta2),
+    //               abs(state.theta2 - alt_test_state.theta2)));
  }
 
 void loop(){
