84be48c29704ad0dcdc533759a9d87e3998b76ff
jingerchong <jingersiachong@gmail.com>
2024-02-15 11:25:45 -0500

    drawing joint space changes

--- src/lab_code/drawing.cpp
+++ src/lab_code/drawing.cpp
@@ -17,11 +17,11 @@ PID motorPID2(3.0, 0.0, 0.5, tau, 0.1, false);
 
 JoystickReading joystick_reading;
 
-JointSpace setpoint = {0, 0};
-JointSpace new_setpoint = {0, 0};
+JointSpace setpoint = {THETA1_OFFSET, 0.0};
+JointSpace new_setpoint;
 
-double position1 = 0; //radians
-double position2 = 0; //radians
+double theta1 = 0; //radians
+double theta2 = 0; //radians
 double controlEffort1 = 0; //duty cycle
 double controlEffort2 = 0; //duty cycle
 
@@ -30,13 +30,12 @@ MotorDriver motor2(DIR2, PWM2, 0);
 EncoderVelocity encoder1(ENCODER1_A_PIN, ENCODER1_B_PIN, CPR_60_RPM, 0.2);
 EncoderVelocity encoder2(ENCODER2_A_PIN, ENCODER2_B_PIN, CPR_60_RPM, 0.2);
 
-
-double alpha = 0.005;
+double alpha = 0.01;
 
 // Checks if provided JointSpace state is within safety limits
 // Returns true if it is and false otherwise
 bool safetyLimit(JointSpace state) {
-    return abs(state.theta1) <= M_PI/2.0 &&
+    return abs(state.theta1 - M_PI/2.0) <= M_PI/2.0 &&
            abs(state.theta2) <= M_PI*0.9;
 };
 
@@ -55,11 +54,17 @@ void loop() {
         // joystick_reading = 
 
         if (JOINT_SPACE) {
-            // TODO 1: Scale joystick_reading from range [-1, 1) to range [-M_PI/2.0, M_PI/2.0)
+            // TODO 1: Scale joystick_reading from range [-1, 1) to range [-M_PI/3.0, M_PI/3.0)
             new_setpoint.theta1 = 0;
             new_setpoint.theta2 = 0;
+
+            // Translates the setpoint to the defined coordinate system
+            new_setpoint.theta1 += THETA1_OFFSET;
+            new_setpoint.theta2 = -new_setpoint.theta2;
         } else {
             // TODO 2: Set new_setpoint using inverseKinematics() on joystick_reading
+            // Make sure your endEffector coordinates are bounded within a reasonable range
+            // e.g. x in [-0.3, 0.3], y in [0.1, 0.3]
             TaskSpace endEffector;
             // endEffector.x = 
             // endEffector.y = 
@@ -78,10 +83,10 @@ void loop() {
     // Update PID at 10kHz
     EVERY_N_MICROS(100) {        
 
-        position1 = encoder1.getPosition() + THETA1_OFFSET;
-        position2 = -encoder2.getPosition();
-        controlEffort1 = motorPID1.calculateParallel(setpoint.theta1, position1);
-        controlEffort2 = motorPID2.calculateParallel(setpoint.theta2, position2);
+        theta1 = encoder1.getPosition() + THETA1_OFFSET;
+        theta2 = -encoder2.getPosition();
+        controlEffort1 = motorPID1.calculateParallel(theta1, setpoint.theta1);
+        controlEffort2 = motorPID2.calculateParallel(theta2, setpoint.theta2);
 
         motor1.drive(controlEffort1);
         motor2.drive(controlEffort2);
@@ -91,10 +96,10 @@ void loop() {
     EVERY_N_MILLIS(100) {
 
             // Print values to serial monitor
-            Serial.printf("MOTOR 1 Setpoint (rad): %.2f, Position (rad): %.2f, Control Effort: %.2f\n",
-                          setpoint.theta1, position1, controlEffort1);
-            Serial.printf("MOTOR 2 Setpoint (rad): %.2f, Position (rad): %.2f, Control Effort: %.2f\n",
-                          setpoint.theta2, position2, controlEffort2);
+            Serial.printf("T1 SP (rad): %.2f, POS (rad): %.2f, CE: %.2f,   "
+                          "T2 SP (rad): %.2f, POS (rad): %.2f, CE: %.2f\n",
+                          setpoint.theta1, theta1, controlEffort1,
+                          setpoint.theta2, theta2, controlEffort2);
 
     }
 }
